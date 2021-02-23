fc26d5e6e7785f16234a429ad53e307da1fa5925
Phillip Doise <p.doise@gmail.com>
2021-02-22 20:49:03 -0700

    add camera functionality

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,17 @@
 import { Component } from "@angular/core";
 import { RouterExtensions } from '@nativescript/angular';
 
+import { Image } from "@nativescript/core";
+import * as camera from '@nativescript/camera';
+
 @Component({
   selector: "pidro-profile",
   templateUrl: "./profile.component.html",
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  name: string = 'Fila Fill';
+  image: string = '';
 
   constructor(private router: RouterExtensions) {}
 
@@ -16,4 +21,20 @@ export class ProfileComponent {
       transition: { name: 'fade' }
     });
   }
+
+  takePicture() {
+    camera.requestPermissions().then(() => {
+      camera.takePicture()
+        .then((imageAsset) => {
+          console.log("Result is an image asset instance");
+          var image = new Image();
+          image.src = imageAsset;
+          console.log(image.src)
+        }).catch((err) => {
+          console.log("Error -> " + err.message);
+        });
+      },
+    )
+  }
+
 }
